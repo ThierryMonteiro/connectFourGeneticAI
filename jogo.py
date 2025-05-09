88fcb2107f11695c174acc314d4f85cd1de513db
thierrymonteiro <thierrywmon@gmail.com>
2025-05-08 23:36:25 -0300

    Melhoria na lógica de validação de fim do jogo, criação do método que lista as ações possíveis

--- jogo.py
+++ jogo.py
@@ -42,7 +42,8 @@ class Jogo():
 
         self.turno += 1
         self.imprimeTabuleiro()
-        return True
+        self.jogador = 2 if self.jogador == 1 else 1
+        return self
 
     def findLastZero(self, coluna): # retorna a linha do último zero na coluna
 
@@ -55,44 +56,46 @@ class Jogo():
 
         # Verifica se há 4 peças em linha, baseadas na última jogada
 
+        verificaJogador = 2 if self.jogador == 1 else 1
+
         # Verifica na horizontal
         for i in range(self.largura-3):
-            if self.estado[self.ultimaJogada[0]][i] == self.jogador and \
-                    self.estado[self.ultimaJogada[0]][i+1] == self.jogador and \
-                    self.estado[self.ultimaJogada[0]][i+2] == self.jogador and \
-                    self.estado[self.ultimaJogada[0]][i+3] == self.jogador:
-                print("Jogador " + str(self.jogador) + " ganhou fazendo uma jogada na linha " + str(self.ultimaJogada[0]))
+            if self.estado[self.ultimaJogada[0]][i] == verificaJogador and \
+                    self.estado[self.ultimaJogada[0]][i+1] == verificaJogador and \
+                    self.estado[self.ultimaJogada[0]][i+2] == verificaJogador and \
+                    self.estado[self.ultimaJogada[0]][i+3] == verificaJogador:
+                print("Jogador " + str(verificaJogador) + " ganhou fazendo uma jogada na linha " + str(self.ultimaJogada[0]))
                 return True
 
         # Verifica na vertical
         for i in range(self.altura-3):
-            if self.estado[i][self.ultimaJogada[1]] == self.jogador and \
-                    self.estado[i+1][self.ultimaJogada[1]] == self.jogador and \
-                    self.estado[i+2][self.ultimaJogada[1]] == self.jogador and \
-                    self.estado[i+3][self.ultimaJogada[1]] == self.jogador:
-                print("Jogador " + str(self.jogador) + " ganhou fazendo uma jogada na coluna " + str(self.ultimaJogada[1]))
+            if self.estado[i][self.ultimaJogada[1]] == verificaJogador and \
+                    self.estado[i+1][self.ultimaJogada[1]] == verificaJogador and \
+                    self.estado[i+2][self.ultimaJogada[1]] == verificaJogador and \
+                    self.estado[i+3][self.ultimaJogada[1]] == verificaJogador:
+                print("Jogador " + str(verificaJogador) + " ganhou fazendo uma jogada na coluna " + str(self.ultimaJogada[1]))
                 return True
 
 
         # Verifica na diagonal principal
         for i in range(self.altura-3):
             for j in range(self.largura-3):
-                if self.estado[i][j] == self.jogador and \
-                        self.estado[i+1][j+1] == self.jogador and \
-                        self.estado[i+2][j+2] == self.jogador and \
-                        self.estado[i+3][j+3] == self.jogador:
-                    print("Jogador " + str(self.jogador) + " ganhou fazendo uma jogada na diagonal principal")
+                if self.estado[i][j] == verificaJogador and \
+                        self.estado[i+1][j+1] == verificaJogador and \
+                        self.estado[i+2][j+2] == verificaJogador and \
+                        self.estado[i+3][j+3] == verificaJogador:
+                    print("Jogador " + str(verificaJogador) + " ganhou fazendo uma jogada na diagonal principal")
                     return True
 
         # Verifica na diagonal secundária
 
         for i in range(3, self.altura):
             for j in range(self.largura-3):
-                if self.estado[i][j] == self.jogador and \
-                        self.estado[i-1][j+1] == self.jogador and \
-                        self.estado[i-2][j+2] == self.jogador and \
-                        self.estado[i-3][j+3] == self.jogador:
-                    print("Jogador " + str(self.jogador) + " ganhou fazendo uma jogada na diagonal secundária")
+                if self.estado[i][j] == verificaJogador and \
+                        self.estado[i-1][j+1] == verificaJogador and \
+                        self.estado[i-2][j+2] == verificaJogador and \
+                        self.estado[i-3][j+3] == verificaJogador:
+                    print("Jogador " + str(verificaJogador) + " ganhou fazendo uma jogada na diagonal secundária")
                     return True
 
 
@@ -102,9 +105,13 @@ class Jogo():
             print("Empate!")
             return True
 
-        if self.jogador == 1:
-            self.jogador = 2
-        else:
-            self.jogador = 1
-
         return False
+
+    def actions(self):
+        actions = []
+        for i in range(len(self.estado)):
+            if self.estado[0][i] == 0:
+                actions.append(i)
+        return actions
+
+
